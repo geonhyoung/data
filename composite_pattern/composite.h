54100eca3a5e408255bb50b9338a67664f408017
geonhyoung_joe <geonhyoung_joe@ghjoe.internal>
2018-09-03 15:11:43 +0900

    sample code

---
+++ composite_pattern/composite.h
@@ -0,0 +1,60 @@
+#ifndef COMPOSITE_PATTEN_H
+#define COMPOSITE_PATTEN_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+
+using namespace std;
+
+namespace Composite_pattern {
+    class Node {
+        public:
+            Node () {}
+            virtual void print () const = 0;
+            virtual ~Node () {}
+    };
+
+    class File: public Node {
+        public:
+            File (string name): name_(name) {}
+            void print () const {
+                cout << name_ << endl;
+            }
+            ~File () {}
+        private:
+            string name_;
+    };
+
+    class Directory: public Node {
+        public:
+            Directory (string name): name_(name) {}
+            void print () const {
+                cout << name_ << endl;
+                cout << "---------------------------------------" << endl;
+
+                /*
+                for (vector<Node*>::iterator iter = vec.begin();
+                        iter != vec.end(); iter++)
+                    (*iter)->print();
+                    */
+                for (int i = 0; i < vec.size(); i++)
+                    vec[i]->print();
+
+                cout << "---------------------------------------" << endl;
+
+            }
+            void add (Node *node) {
+                vec.push_back (node);
+            }
+            ~Directory () {
+                for (int i = 0; i < vec.size(); i++)
+                    delete vec[i];
+            }
+        private:
+            string name_;
+            vector<Node*> vec;
+    };
+};
+#endif // COMPOSITE_PATTEN_H
