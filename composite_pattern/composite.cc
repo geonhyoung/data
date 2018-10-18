3154d0af6da6c1d0b8d61dadcca048e260670d06
geonhyeong <9310121@naver.com>
2018-10-18 22:05:19 +0900

    google coding sytle

--- composite_pattern/composite.cc
+++ composite_pattern/composite.cc
@@ -2,30 +2,30 @@
 
 using namespace Composite_pattern;
 
-int main () {
-    File *f1 = new File ("file1");
-    File *f2 = new File ("file2");
-    File *f3 = new File ("file3");
-    File *f4 = new File ("file4");
-    File *f5 = new File ("file5");
-    File *f6 = new File ("file6");
-    File *f7 = new File ("file7");
+int main() {
+  File *f1 = new File("file1");
+  File *f2 = new File("file2");
+  File *f3 = new File("file3");
+  File *f4 = new File("file4");
+  File *f5 = new File("file5");
+  File *f6 = new File("file6");
+  File *f7 = new File("file7");
 
-    Directory *d1 = new Directory ("directory1");
-    Directory *d2 = new Directory ("directory2");
+  Directory *d1 = new Directory("directory1");
+  Directory *d2 = new Directory("directory2");
 
-    d1->add (f1);
-    d1->add (f2);
-    d1->add (f3);
+  d1->add(f1);
+  d1->add(f2);
+  d1->add(f3);
 
-    d2->add (f4);
-    d2->add (f5);
-    d2->add (f6);
+  d2->add(f4);
+  d2->add(f5);
+  d2->add(f6);
 
-    d1->add (d2);
-    d1->add (f7);
+  d1->add(d2);
+  d1->add(f7);
 
-    d1->print();
+  d1->print();
 
-    return 0;
+  return 0;
 }
--- composite_pattern/composite.h
+++ composite_pattern/composite.h
@@ -2,59 +2,54 @@
 #define COMPOSITE_PATTEN_H
 
 #include <iostream>
+#include <iterator>
 #include <string>
 #include <vector>
-#include <iterator>
 
 using namespace std;
 
 namespace Composite_pattern {
-    class Node {
-        public:
-            Node () {}
-            virtual void print () const = 0;
-            virtual ~Node () {}
-    };
-
-    class File: public Node {
-        public:
-            File (string name): name_(name) {}
-            void print () const {
-                cout << name_ << endl;
-            }
-            ~File () {}
-        private:
-            string name_;
-    };
-
-    class Directory: public Node {
-        public:
-            Directory (string name): name_(name) {}
-            void print () const {
-                cout << name_ << endl;
-                cout << "---------------------------------------" << endl;
-
-                /*
-                for (vector<Node*>::iterator iter = vec.begin();
-                        iter != vec.end(); iter++)
-                    (*iter)->print();
-                    */
-                for (int i = 0; i < vec.size(); i++)
-                    vec[i]->print();
-
-                cout << "---------------------------------------" << endl;
-
-            }
-            void add (Node *node) {
-                vec.push_back (node);
-            }
-            ~Directory () {
-                for (int i = 0; i < vec.size(); i++)
-                    delete vec[i];
-            }
-        private:
-            string name_;
-            vector<Node*> vec;
-    };
+class Node {
+ public:
+  Node() {}
+  virtual void print() const = 0;
+  virtual ~Node() {}
 };
-#endif // COMPOSITE_PATTEN_H
+
+class File : public Node {
+ public:
+  File(string name) : name_(name) {}
+  void print() const { cout << name_ << endl; }
+  ~File() {}
+
+ private:
+  string name_;
+};
+
+class Directory : public Node {
+ public:
+  Directory(string name) : name_(name) {}
+  void print() const {
+    cout << name_ << endl;
+    cout << "---------------------------------------" << endl;
+
+    /*
+    for (vector<Node*>::iterator iter = vec.begin();
+            iter != vec.end(); iter++)
+        (*iter)->print();
+        */
+    for (unsigned int i = 0; i < vec.size(); i++) vec[i]->print();
+
+    cout << "---------------------------------------" << endl;
+  }
+  void add(Node *node) { vec.push_back(node); }
+  ~Directory() {
+    for (unsigned int i = 0; i < vec.size(); i++) delete vec[i];
+  }
+
+ private:
+  string name_;
+  vector<Node *> vec;
+};
+};      // namespace Composite_pattern
+#endif  // COMPOSITE_PATTEN_H
